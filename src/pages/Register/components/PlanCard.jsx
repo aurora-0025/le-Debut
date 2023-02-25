63f31a867f7239cf42e08060b8e6df30b9edb378
Ritin George <78461388+aurora-0025@users.noreply.github.com>
2023-02-25 22:27:32 +0530

    fix planCard height

--- src/pages/Register/components/PlanCard.jsx
+++ src/pages/Register/components/PlanCard.jsx
@@ -2,30 +2,32 @@ import React from 'react'
 import './PlanCard.css'
 import { ReactComponent as Rupee } from '../../../assets/images/rupee.svg'
 
-function PlanCard({plan, price, desc, setPlan}) {
+function PlanCard({ plan, price, desc, setPlan }) {
     return (
         <div className='planCard'>
             <div className='CardContent'>
-                <h3>two day pass</h3>
-                <h4>{plan}</h4>
-                <div className='price'>
-                    <Rupee className='rupee' />
-                    <h2>{price}</h2>
+                <div className='cardHead'>
+                    <h3>two day pass</h3>
+                    <h4>{plan}</h4>
+                    <div className='price'>
+                        <Rupee className='rupee' />
+                        <h2>{price}</h2>
+                    </div>
+                    <hr />
                 </div>
-                <hr />
-                <div className="desc">
-                    {desc}
+                <div className='cardFooter'>
+                    <div className='desc'>{desc}</div>
+                    <button
+                        type='submit'
+                        className='submit'
+                        onClick={(e) => {
+                            e.preventDefault()
+                            setPlan(plan)
+                        }}
+                    >
+                        <p>{plan === 'excl. food pass' ? 'No food for me (:' : 'Select'}</p>
+                    </button>
                 </div>
-                <button
-                    type='submit'
-                    className='submit'
-                    onClick={(e) => {
-                        e.preventDefault()
-                        setPlan(plan);
-                    }}
-                >
-                    <p>{plan === 'excl. food pass' ? 'No food for me (:': 'Select'}</p>
-                </button>
             </div>
         </div>
     )
